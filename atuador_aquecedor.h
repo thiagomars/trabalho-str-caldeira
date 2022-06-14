99d816d7aa2277bfe4fb33427b7bc0ce6ec27a4b
Thiago Marques Sousa <50245545+thiagomars@users.noreply.github.com>
2022-06-14 14:18:06 -0300

    Add files via upload
    monitores do atuadores

---
+++ atuador_aquecedor.h
@@ -0,0 +1,6 @@
+#ifndef REFNIVEL_H
+#define REFNIVEL_H
+
+void atuador_put_aquecedor(double abertura);
+
+#endif
---
+++ atuador_fluxo_aquecida.h
@@ -0,0 +1,8 @@
+#ifndef ATUADOR_FLUXO_AQUECEDA_H
+#define ATUADOR_FLUXO_AQUECEDA_H
+
+
+void atuador_put_fluxo_aquecida(double abertura);
+
+
+#endif
