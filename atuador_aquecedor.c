bc0c73b9f163e2a0e55a7b229faca0402ee4a63d
Matheus Soares <97004511+JMatheusSF@users.noreply.github.com>
2022-06-15 20:08:35 -0300

    Update atuador_aquecedor.c

--- atuador_aquecedor.c
+++ atuador_aquecedor.c
@@ -16,7 +16,7 @@ void atuador_put_aquecedor(double abertura){
 
 double atuador_get_aquecedor(void){
 	double aux;
-	thread_mutex_lock( &exclusao_mutua); //Exclusão mútua dos recursos
+	pthread_mutex_lock( &exclusao_mutua); //Exclusão mútua dos recursos
 	aux = comporta;
 	pthread_mutex_unlock( &exclusao_mutua);//Libera os recursos exclusos
 	return aux;
